6b5898fc1817b68d702b77a3ec5ea5a67f650bc4
Santiago Toyos <ppikachu@gmail.com>
2022-06-26 05:58:41 -0300

    piezas 04

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, onBeforeMount } from 'vue'
 
 import {
   Renderer,
@@ -11,8 +11,9 @@ import {
   AmbientLight,
 } from 'troisjs'
 import { ShockWaveEffect, VignetteEffect, BloomEffect, EffectComposer, EffectPass, RenderPass, BrightnessContrastEffect, BlendFunction } from 'postprocessing'
-import { AnimationMixer, Clock, TextureLoader, EquirectangularReflectionMapping, sRGBEncoding, Vector3 } from 'three'
+import { AnimationMixer, Clock, TextureLoader, EquirectangularReflectionMapping, sRGBEncoding, ACESFilmicToneMapping, Vector3, FloatType, PMREMGenerator } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 
 import imgUrl from './assets/images/2294472375_24a3b8ef46_o.jpeg'
 
@@ -34,13 +35,18 @@ const target = new Vector3(0, 0, 0)
 var clock = new Clock()
 let mixer, textureEquirec, sphereMesh, sphereMaterial, composer, camera, renderer, scene, shockWaveEffect, hit
 
-onMounted(() => {
+onBeforeMount(() => {
   console.clear()
+})
+
+onMounted(() => {
   renderer = rendererRef.value.renderer
   scene = sceneRef.value.scene
   camera = cameraRef.value.camera
-  renderer.outputEncoding = sRGBEncoding
+  
   // postprocessing
+  renderer.outputEncoding = sRGBEncoding
+  //renderer.toneMapping = ACESFilmicToneMapping
   shockWaveEffect = new ShockWaveEffect(camera, target, {
     speed: 1.25,
     maxRadius: 0.01,
@@ -49,12 +55,12 @@ onMounted(() => {
   })
   const bloomEffect = new BloomEffect({
     //blendFunction: BlendFunction.SCREEN,
-    luminanceThreshold: 0.1,
+    luminanceThreshold: 0.2,
     intensity: 0.5,
     radius: 0.8,
   })
   const brightnessContrastEffect = new BrightnessContrastEffect({
-    blendFunction: BlendFunction.SCREEN,
+    //blendFunction: BlendFunction.SCREEN,
     brightness: 0.3,
     contrast: 0.6,
   })
@@ -66,9 +72,11 @@ onMounted(() => {
     brightnessContrastEffect,
     //vignetteEffect
   )
+
+  // apply postprocessing
   composer = new EffectComposer(renderer)
-  composer.addPass(renderPass)
-  composer.addPass(effectPass)
+  //composer.addPass(renderPass)
+  //composer.addPass(effectPass)
 
   // piezas
   const loader = new GLTFLoader()
@@ -77,23 +85,28 @@ onMounted(() => {
     const piezaHeavy = model.children[0].children[0]
     const piezaLight = model.children[1].children[0]
     scene.add(model)
-    console.log(rendererRef.value.renderer)
+    //console.log(rendererRef.value.renderer)
     // self shadowing
     piezaHeavy.castShadow = true
     piezaHeavy.receiveShadow = true
     piezaLight.castShadow = true
     piezaLight.receiveShadow = true
     // environment
-    const textureLoader = new TextureLoader()
-    textureEquirec = textureLoader.load(imgUrl)
-    textureEquirec.mapping = EquirectangularReflectionMapping
-    textureEquirec.encoding = sRGBEncoding
+    const hdrimgUrl = '/images/sample.hdr'
 
-    //scene.background = textureEquirec
+    const loadedTexture = new TextureLoader().load(imgUrl)
+    //const loadedTexture = new RGBELoader().load(hdrimgUrl)
+    //loadedTexture.setDataType(FloatType)
+    const gen = new PMREMGenerator(renderer)
+    const texture = loadedTexture
+    //const texture = gen.fromEquirectangular(loadedTexture).texture
+    texture.mapping = EquirectangularReflectionMapping
+    texture.encoding = sRGBEncoding
+    //console.log(texture)
 
-    piezaHeavy.material.envMap = textureEquirec
-    piezaLight.material.envMap = textureEquirec
-    //piezaLight.material.needsUpdate = true
+    scene.background = texture
+    piezaHeavy.material.envMap = texture
+    piezaLight.material.envMap = texture
 
     // animation
     hit = true
@@ -103,7 +116,7 @@ onMounted(() => {
       shockWaveEffect.explode()
       hit = true
     })
-    console.log(mixer)
+    //console.log(mixer)
     animate()
   },
   undefined, function (e) {
@@ -121,7 +134,8 @@ function animate() {
     shockWaveEffect.explode()
     hit = false
   }
-  composer.render()
+  //FX render pass
+  //composer.render()
 }
 
 // Resizing
@@ -144,7 +158,7 @@ function onResize(event) {
     <Camera ref="cameraRef" :position="{ x: 1, y: 2, z: 10 }" />
     <Scene ref="sceneRef">
       <!--<AmbientLight ref="light" />-->
-      <!--<PointLight ref="light" color="#ffffff" :intensity="5" :distance="30" :position="{ x:1, y:15, z:15 }" cast-shadow :shadow-map-size="{ width: 1024, height: 1024 }" />-->
+      <PointLight ref="light" color="#ffffff" :intensity="5" :distance="30" :position="{ x:1, y:15, z:15 }" cast-shadow :shadow-map-size="{ width: 1024, height: 1024 }" />
       <!--<Plane ref="plane" :width="PARAMS.planeSize" :height="PARAMS.planeSize" :rotation="{ x: -Math.PI / 2 }" :position="{y: -4}" receive-shadow>
         <StandardMaterial color="#2299EE" />
       </Plane>-->
